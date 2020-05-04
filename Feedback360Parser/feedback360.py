8f39a2c009cb37317742b146ee6d52052215a197
andre.marcos <amsilva@gmail.com>
2020-05-04 03:12:00 -0300

    class Avaliado, correcao path para arquivo saida

--- Feedback360Parser/feedback360.py
+++ Feedback360Parser/feedback360.py
@@ -1,32 +1,6 @@
 
 import csv
-
-#class
-class Avaliado:
-
-    def __init__(self, ra):
-        self.ra = ra
-        self.media = 0
-        self.cont = 0
-        self.total = 0
-
-    def __str__(self):
-        return self.ra
-
-    def raGet(self):
-        return self.ra
-
-    def mediaGet(self):
-        return self.media
-
-    def contGet(self):
-        return self.cont
-
-    def avalia(self, comunic, compro, resul):
-        self.cont = self.cont + 1
-        self.total = self.total + int(comunic) + int(compro) + int(resul)
-        self.media = self.total / (self.cont * 3)
-
+from avaliado import Avaliado 
 
 arquivoin = open('./Feedback360Parser/entrada.csv')
 alunoslist = csv.DictReader(arquivoin)
@@ -56,10 +30,10 @@ venabled = 'enabled'
 vtype = 'team'
 
 
-print("[user]\n")
+print("[parsing...]\n")
 for alunocsv in alunoslist:
     
-    print("AVALIADOR =", alunocsv["RA"]) 
+    print("\nAVALIADOR =", alunocsv["RA"]) 
     print("PROJETO =", alunocsv["PROJETO"])
 
     for i in (1, 2, 3, 4, 5) :
@@ -67,16 +41,10 @@ for alunocsv in alunoslist:
         existe = avaliadoGetByRa(alunocsv["NOME"+str(i)])
 
         if(existe == None) :
-            print("NOVO ----->",alunocsv["NOME"+str(i)])
-            print(alunocsv["COMUNICACAO"+str(i)], alunocsv["COMPROMETIMENTO"+str(i)], alunocsv["RESULTADO"+str(i)])
-
             novo = Avaliado(alunocsv["NOME"+str(i)])
             novo.avalia(alunocsv["COMUNICACAO"+str(i)], alunocsv["COMPROMETIMENTO"+str(i)], alunocsv["RESULTADO"+str(i)])
             avaliados.append(novo)
         else :
-            print("ENVONTRADO ----->",alunocsv["NOME"+str(i)])
-            print(alunocsv["COMUNICACAO"+str(i)], alunocsv["COMPROMETIMENTO"+str(i)], alunocsv["RESULTADO"+str(i)])
-
             existe.avalia(alunocsv["COMUNICACAO"+str(i)], alunocsv["COMPROMETIMENTO"+str(i)], alunocsv["RESULTADO"+str(i)])
 
         if (i==5) : 
@@ -85,19 +53,16 @@ for alunocsv in alunoslist:
             if(alunocsv["MAIS"+str(i)] == "No"):
                 break
 
-
 #relatorio console
 print("\nRelatorio all ---------------") 
 for avaliado in avaliados:
     print(avaliado, "-", avaliado.mediaGet(), "com", avaliado.total, "de", avaliado.contGet())
 
 #file
-arquivoout = open('./feedback360parse/saida.txt', 'w')
+arquivoout = open('./Feedback360Parser/saida.txt', 'w')
 arquivoout.write("Relatorio all ---------------\n")
 for avaliado in avaliados:
     imprime = avaliado.raGet()+ "-"+ str(avaliado.mediaGet())+ "de" + str(avaliado.contGet())
     arquivoout.write(imprime)
 
-
-
-print("**** FIM")
+print("\n**** FIM")
